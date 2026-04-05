0fd59ee1c658b5704e5710ac655edd68f3fd791e
Jacob Ferreira <jferre59@my.centennialcollege.ca>
2026-04-05 14:45:10 -0400

    Create fixtures.py
    Script containing reusable valid payload for testing

---
+++ backend/tests/fixtures.py
@@ -0,0 +1,14 @@
+"""Provides a valid payload for testing the API endpoints."""
+
+
+VALID_PAYLOAD = {
+    "age": 35,
+    "symptom_1": "cough",
+    "symptom_2": "fever",
+    "symptom_3": "NONE",
+    "sex": "male",
+    "nature": "medium",
+    "age_group": "adult",
+    "symptom_count": 2,
+    "high_risk": "no"
+}
